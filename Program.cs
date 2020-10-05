49258bebea0aaa4d6703d501239211eb92c20bf5
dheer meena <dheer1998meena@gmail.com>
2020-10-05 08:19:39 +0530

    [Dheer] Add . added  UC6

--- Program.cs
+++ Program.cs
@@ -18,7 +18,8 @@ namespace AddressBookSystem
                 Console.WriteLine("2. View all Contacts.");
                 Console.WriteLine("3.Edit existing contacts.");
                 Console.WriteLine("4.Remove a contact.");
-                Console.WriteLine("5.Exit.");
+                Console.WriteLine("5.View AddressBook for a key name.");
+                Console.WriteLine("6.Exit.");
                 choice = Convert.ToInt32(Console.ReadLine());
 
                 if (choice == 1)
@@ -38,10 +39,11 @@ namespace AddressBookSystem
                     String contactNo = Console.ReadLine();
                     Console.WriteLine("Enter your email : ");
                     String mailID = Console.ReadLine();
-
+                    Console.WriteLine("Enter the key name to be saved in the address book : ");
+                    String keyname = Console.ReadLine();
                     Contact c = new Contact(name, address, city, state, zip, contactNo, mailID);
 
-                    ab.AddAddress(c);
+                    ab.AddAddress(keyname, c);
                 }
                 else if (choice == 2)
                 {
@@ -71,11 +73,24 @@ namespace AddressBookSystem
                     String rname = Console.ReadLine();
                     ab.RemoveContact(rname);
                 }
+                else if (choice == 5)
+                {
+                    Console.WriteLine("Enter the key name : ");
+                    String kname = Console.ReadLine();
+                    Contact cc = ab.ViewByKeyName(kname);
+                    Console.WriteLine("Name : " + cc.GetName());
+                    Console.WriteLine("Address : " + cc.GetAddress());
+                    Console.WriteLine("City : " + cc.GetCity());
+                    Console.WriteLine("State : " + cc.GetState());
+                    Console.WriteLine("zip : " + cc.GetZip());
+                    Console.WriteLine("Contact No. : " + cc.GetPhoneNo());
+                    Console.WriteLine("Email ID : " + cc.GetEmail());
+                }
                 else
                 {
                     break;
                 }
-            } while (choice != 5);
+            } while (choice != 6);
         }
     }
     
